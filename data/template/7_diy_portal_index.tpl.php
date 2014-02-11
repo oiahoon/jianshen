0b8187810795a86152eeb304945027719b5e939a
huangyuyao <huangyuyao@cd.tuan800.com>
2014-02-11 19:37:41 +0800

    modify

--- data/template/7_diy_portal_index.tpl.php
+++ data/template/7_diy_portal_index.tpl.php
@@ -1,5 +1,5 @@
 <?php if(!defined('IN_DISCUZ')) exit('Access Denied'); hookscriptoutput('index');
-block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,130,131,132,119,120,121,122,123,124,127,128,129');?><?php include template('common/header'); ?><script src="template/jianshen/images/jquery.slides.min.js" type="text/javascript" ></script>
+block_get('97,148,149,150,151,125,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,130,131,132,119,120,121,122,123,124,127,128,129');?><?php include template('common/header'); ?><script src="template/jianshen/images/jquery.slides.min.js" type="text/javascript" ></script>
 <script src="template/jianshen/images/jianshen.js" type="text/javascript" ></script>
 <script src="template/jianshen/images/jianshen_gundong.js" type="text/javascript" ></script>
 <?php if($_GET['diy'] == 'yes' && check_diy_perm($topic)) { ?>
@@ -9,48 +9,158 @@ block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,
 <?php } else { } ?>
 </div>
 <div class="hdbg">
-<div class="w_950">
-<!-- 轮播 S 1420--><div class="hotmain_l" id="slides"><div class="hotmain_l_bg"></div>
-
-<!--[diy=jianshen_door_010_01]--><div id="jianshen_door_010_01" class="area"><div id="frameiN6J09" class="frame move-span cl frame-1"><div id="frameiN6J09_left" class="column frame-1-c"><div id="frameiN6J09_left_temp" class="move-span temp"></div><?php block_display('97');?></div></div></div><!--[/diy]-->
-
-
-</div>
-    	<!-- 轮播右侧 S 1421--><div class="hotmain_r">
-<div class="hotmain_r_bg"></div>
-<img border="0" style="" class="jrymhy" usemap="#Map" alt="" src="template/jianshen/images/jrymhy.gif">
-<map id="Map" name="Map"><area target="_self" href="member.php?mod=register" coords="67,280,239,323" shape="rect"></map>
-
-</div><!-- 轮播右侧 e -->
-</div>
+  <div class="w_950">
+    <!-- 轮播 S 1420-->
+    <div class="hotmain_l" id="slides">
+      <div class="hotmain_l_bg"></div>
+      <!--[diy=jianshen_door_010_01]--><div id="jianshen_door_010_01" class="area"><div id="frameiN6J09" class="frame move-span cl frame-1"><div id="frameiN6J09_left" class="column frame-1-c"><div id="frameiN6J09_left_temp" class="move-span temp"></div><?php block_display('97');?></div></div></div><!--[/diy]-->
+    </div>
+    <!-- 轮播右侧 S 1421-->
+    <div class="hotmain_r">
+      <div class="hotmain_r_bg"></div>
+      <img border="0" style="" class="jrymhy" usemap="#Map" alt="" src="template/jianshen/images/jrymhy.gif">
+      <map id="Map" name="Map"><area target="_self" href="member.php?mod=register" coords="67,280,239,323" shape="rect"></map>
+    </div>
+    <!-- 轮播右侧 e -->
+  </div>
 </div>
 <!-- 轮播 E -->
 
 
 
 
-<div class="wp">
-<div id="zdkc" class="column columnbug">
-    <div class="topbg2"></div>
-    <h2><span><a href="http://www.oiahoon.com/" target="_blank">oiahoon</a> 健身平台！</span><em class="h2l"></em></h2>
-    <div class="column_c">
-   
-<ul id="course_zdkc" class="zdkclist">
-<!--[diy=jianshen_door_010_03]--><div id="jianshen_door_010_03" class="area"><div id="frameUVN3ho" class="frame move-span cl frame-1"><div id="frameUVN3ho_left" class="column frame-1-c"><div id="frameUVN3ho_left_temp" class="move-span temp"></div><?php block_display('98');?></div></div></div><!--[/diy]-->
-</ul>
+<div class="wp"><!-- 主体开始 -->
 
-    <!-- 活动-->
-<div class="wydkc cl" style="clear:both"><!--[diy=jianshen_door_010_04]--><div id="jianshen_door_010_04" class="area"><div id="framezT88Z6" class="frame move-span cl frame-1"><div id="framezT88Z6_left" class="column frame-1-c"><div id="framezT88Z6_left_temp" class="move-span temp"></div><?php block_display('99');?></div></div></div><!--[/diy]-->
+<!-- 三个大图 -->
+<div class="produced">
+  <ul class="clr">
+   <!--[diy=jianshen_door_010_101]--><div id="jianshen_door_010_101" class="area"><div id="frame2z941z" class="frame move-span cl frame-1"><div id="frame2z941z_left" class="column frame-1-c"><div id="frame2z941z_left_temp" class="move-span temp"></div><?php block_display('148');?></div></div></div><!--[/diy]-->
+  </ul>
 </div>
+<!-- .三个大图 -->
 
-<ul id="course_xyd" style="clear:both">
-<!--[diy=jianshen_door_010_05]--><div id="jianshen_door_010_05" class="area"><div id="frameUYrGRp" class="frame move-span cl frame-1"><div id="frameUYrGRp_left" class="column frame-1-c"><div id="frameUYrGRp_left_temp" class="move-span temp"></div><?php block_display('126');?></div></div></div><!--[/diy]-->
-</ul>
- 
+
+<!-- 左侧编号 jianshen_door_010_1XX -->
+<!-- 右侧编号 jianshen_door_010_2XX -->
+
+<div class="left_block">
+  <!-- 公告 -->
+  <div id="notice" class="column">
+    <div class="column_c">
+      <div class="notice">
+        <!--[diy=jianshen_door_010_102]--><div id="jianshen_door_010_102" class="area"><div id="frame44o8e4" class="frame move-span cl frame-1"><div id="frame44o8e4_left" class="column frame-1-c"><div id="frame44o8e4_left_temp" class="move-span temp"></div><?php block_display('149');?></div></div></div><!--[/diy]-->
+      </div>
     </div>
   </div>
-  <div class="xkll">
-    <h2><font style="font-size:18px; color:#333333; font-weight:bold;">滚动新闻</font> 每周都有更新</h2>
+  <!-- .公告 -->
+
+  <!-- 版块1 -->
+  <div id="forum1" class="column">
+    <!-- 标题 -->
+    <div class="topbg2"></div>
+    <h2>
+      <!--[diy=jianshen_door_010_106]--><div id="jianshen_door_010_106" class="area"><div id="frameys1f24" class="frame move-span cl frame-1"><div id="frameys1f24_left" class="column frame-1-c"><div id="frameys1f24_left_temp" class="move-span temp"></div><?php block_display('150');?></div></div></div><!--[/diy]-->
+      
+      <em class="h2l"></em>
+    </h2>
+    <span class="menu_r">
+      <a href="#">性感</a><a href="#">蓝天</a><a href="#">草原</a><a href="#">户外</a>
+    </span>
+    <!-- 列表 -->
+    <div class="column_c">
+      <!--[diy=jianshen_door_010_103]--><div id="jianshen_door_010_103" class="area"></div><!--[/diy]-->
+    </div>
+  </div>
+  <!-- .版块1 -->
+  <!-- 版块2 -->
+  <div id="forum2" class="column columnbug left_block">
+    <div class="topbg2"></div>
+    <h2>
+      <!--[diy=jianshen_door_010_104]--><div id="jianshen_door_010_104" class="area"><div id="frameh2pNoW" class="frame move-span cl frame-1"><div id="frameh2pNoW_left" class="column frame-1-c"><div id="frameh2pNoW_left_temp" class="move-span temp"></div><?php block_display('151');?></div></div></div><!--[/diy]-->
+      <em class="h2l"></em>
+    </h2>
+    <div class="column_c">
+      <!--[diy=jianshen_door_010_105]--><div id="jianshen_door_010_105" class="area"></div><!--[/diy]-->
+    </div>
+  </div>
+  <!-- .版块2 -->
+
+</div>
+
+
+<div class="right_block">
+  <!-- 广告图片？ -->
+  <div class="right_block">
+    <!--[diy=jianshen_door_010_202]--><div id="jianshen_door_010_202" class="area"></div><!--[/diy]-->
+  </div>
+  <!-- .广告图片 -->
+  <div>
+    <h2><font style="font-size:18px; color:#333333; font-weight:bold;">xxxxxxx</font> sssssss</h2>
+  </div>
+  <div>
+    <h2><font style="font-size:18px; color:#333333; font-weight:bold;">xxxxxxx</font> sssssss</h2>
+  </div>
+  <div>
+    <h2><font style="font-size:18px; color:#333333; font-weight:bold;">xxxxxxx</font> sssssss</h2>
+  </div>
+  <div>
+    <h2><font style="font-size:18px; color:#333333; font-weight:bold;">xxxxxxx</font> sssssss</h2>
+  </div>
+  <div>
+    <h2>
+      <font style="font-size:18px; color:#333333; font-weight:bold;">滚动新闻</font> 每周都有更新
+    </h2>
+    <!-- 滚动新闻 --><div class="newvideolist">
+    <a id="newvideoGoLeft" class="up on" href="javascript:void(0)" target="_self"></a>
+    <a id="newvideoGoRight" class="down on" href="javascript:void(0)" target="_self"></a>
+    <div class="newvideolist_c">
+    <ul id="newvideolist"><!--[diy=jianshen_door_010_06]--><div id="jianshen_door_010_06" class="area"><div id="framePaYJW9" class="frame move-span cl frame-1"><div id="framePaYJW9_left" class="column frame-1-c"><div id="framePaYJW9_left_temp" class="move-span temp"></div><?php block_display('125');?></div></div></div><!--[/diy]--></ul>
+    </div>
+    </div>
+    <!-- 滚动新闻 -->
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- #### expamles ##################################################-->
+<div id="zdkc" class="column columnbug left_block">
+  <div class="topbg2"></div>
+  <h2><span><a href="http://www.oiahoon.com/" target="_blank">oiahoon</a> 健身平台！</span><em class="h2l"></em></h2>
+  <div class="column_c">
+   
+    <ul id="course_zdkc" class="zdkclist">
+    <!--[diy=jianshen_door_010_03]--><div id="jianshen_door_010_03" class="area"><div id="frameUVN3ho" class="frame move-span cl frame-1"><div id="frameUVN3ho_left" class="column frame-1-c"><div id="frameUVN3ho_left_temp" class="move-span temp"></div><?php block_display('98');?></div></div></div><!--[/diy]-->
+    </ul>
+
+    <!-- 活动-->
+    <div class="wydkc cl" style="clear:both"><!--[diy=jianshen_door_010_04]--><div id="jianshen_door_010_04" class="area"><div id="framezT88Z6" class="frame move-span cl frame-1"><div id="framezT88Z6_left" class="column frame-1-c"><div id="framezT88Z6_left_temp" class="move-span temp"></div><?php block_display('99');?></div></div></div><!--[/diy]-->
+    </div>
+
+    <ul id="course_xyd" style="clear:both">
+    <!--[diy=jianshen_door_010_05]--><div id="jianshen_door_010_05" class="area"><div id="frameUYrGRp" class="frame move-span cl frame-1"><div id="frameUYrGRp_left" class="column frame-1-c"><div id="frameUYrGRp_left_temp" class="move-span temp"></div><?php block_display('126');?></div></div></div><!--[/diy]-->
+    </ul>
+ 
+  </div>
+</div>
+
+<div class="right_block">
+  <h2><font style="font-size:18px; color:#333333; font-weight:bold;">滚动新闻</font> 每周都有更新</h2>
     <!-- 滚动新闻 --><div class="newvideolist">
 <a id="newvideoGoLeft" class="up on" href="javascript:void(0)" target="_self"></a>
 <a id="newvideoGoRight" class="down on" href="javascript:void(0)" target="_self"></a>
@@ -172,7 +282,7 @@ block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,
 
 </ul>
      <ul style="display: none;">
- <!--[diy=jianshen_door_010_26]--><div id="jianshen_door_010_26" class="area"><div id="frameW67DHs" class="frame move-span cl frame-1"><div id="frameW67DHs_left" class="column frame-1-c"><div id="frameW67DHs_left_temp" class="move-span temp"></div><?php block_display('130');?></div></div></div><!--[/diy]-->
+ <!--[diy=jianshen_door_010_26]--><div id="jianshen_door_010_26" class="area"><div id="framejob8XW" class="frame move-span cl frame-1"><div class="title frame-title"><span class="titletext">1框架</span></div><div id="framejob8XW_left" class="column frame-1-c"><div id="framejob8XW_left_temp" class="move-span temp"></div></div></div><div id="frameW67DHs" class="frame move-span cl frame-1"><div id="frameW67DHs_left" class="column frame-1-c"><div id="frameW67DHs_left_temp" class="move-span temp"></div><?php block_display('130');?></div></div></div><!--[/diy]-->
 </ul> 
       <ul style="display: none;">
  <!--[diy=jianshen_door_010_27]--><div id="jianshen_door_010_27" class="area"><div id="framet9tRo6" class="frame move-span cl frame-1"><div id="framet9tRo6_left" class="column frame-1-c"><div id="framet9tRo6_left_temp" class="move-span temp"></div><?php block_display('131');?></div></div></div><!--[/diy]-->
@@ -220,8 +330,8 @@ block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,
 <a class="hd_video" target="_blank" href="#">精美图片</a><em class="h2l"></em>
 </h2>
 <span class="menu_r">
-<a href="http://www.oiahoon.com/"><strong>网站模板</strong></a><a href="#">性感</a><a href="#">清纯</a><a href="#">可爱</a>|
-<a href="http://www.oiahoon.com/"><strong>网页模板</strong></a><a href="#">蓝天</a><a href="#">草原</a><a href="#">户外</a>|
+<a href="#">性感</a><a href="#">清纯</a><a href="#">可爱</a>|
+<a href="#">蓝天</a><a href="#">草原</a><a href="#">户外</a>|
 <a href="#"><strong>平面设计</strong></a><a href="#">作品</a><a href="#">欣赏</a><a href="#">创意</a></span>
 </div>
         <div class="sdbox_lr_big video_active clearfix">
@@ -254,14 +364,14 @@ block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,
     </div>
     
     
-    <div class="produced">
-<div class="topbg2"></div>
-<div class="hd_s1">
-<h2 class="hd_produced" style="position: static; width:200px; clear:both">三味书屋</h2>
-</div>
-<ul class="clr">
- <!--[diy=jianshen_door_010_35]--><div id="jianshen_door_010_35" class="area"><div id="frameF5jozs" class="frame move-span cl frame-1"><div id="frameF5jozs_left" class="column frame-1-c"><div id="frameF5jozs_left_temp" class="move-span temp"></div><?php block_display('127');?></div></div></div><!--[/diy]-->
-</ul>
+<div class="produced">
+  <div class="topbg2"></div>
+  <div class="hd_s1">
+    <h2 class="hd_produced" style="position: static; width:200px; clear:both">三味书屋</h2>
+  </div>
+  <ul class="clr">
+   <!--[diy=jianshen_door_010_35]--><div id="jianshen_door_010_35" class="area"><div id="frameF5jozs" class="frame move-span cl frame-1"><div id="frameF5jozs_left" class="column frame-1-c"><div id="frameF5jozs_left_temp" class="move-span temp"></div><?php block_display('127');?></div></div></div><!--[/diy]-->
+  </ul>
 </div>
 
 <div class="buyguide">
@@ -288,5 +398,6 @@ block_get('97,98,99,126,125,100,101,102,103,104,105,106,107,108,109,110,111,112,
 
 </style>
 </div><!--友情链接 -->
+</div>
 </div>
 <div style="height:1px; clear:both"></div><?php include template('common/footer'); ?>
