307b60364cba9377c5cd8f6cfc146e362349ffe7
Oiahoon <4296411@qq.com>
2014-02-17 23:12:25 +0800

    modify

--- template/jianshen/portal/index.php
+++ template/jianshen/portal/index.php
@@ -35,7 +35,7 @@
 <div class="wp"><!-- 主体开始 -->
 
 <!-- 三个大图 -->
-<div class="produced">
+<div class="image_3">
   <ul class="clr">
    <!--[diy=jianshen_door_010_101]--><div id="jianshen_door_010_101" class="area"></div><!--[/diy]-->
   </ul>
@@ -67,11 +67,17 @@
       <em class="h2l"></em>
     </h2>
     <span class="menu_r">
-      <a href="#">性感</a><a href="#">蓝天</a><a href="#">草原</a><a href="#">户外</a>
+      <!--[diy=jianshen_door_010_113]--><div id="jianshen_door_010_113" class="area"></div><!--[/diy]-->
     </span>
     <!-- 列表 -->
     <div class="column_c">
+      <div class="cover">
+        <img src="template/jianshen/images/cover/cover_1.png">
+        <span>一些简介在这里一些简介在这里一些简介在这里一些简介在这里</span>
+      </div>
+      <div class="article_list">
       <!--[diy=jianshen_door_010_103]--><div id="jianshen_door_010_103" class="area"></div><!--[/diy]-->
+      </div>
     </div>
   </div> <!-- .版块1 -->
 
@@ -150,283 +156,63 @@
 
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- #### expamles ##################################################-->
-<div id="zdkc" class="column columnbug left_block">
+<div class="column columnbug">
   <div class="topbg2"></div>
-  <h2><span><a href="http://www.oiahoon.com/" target="_blank">oiahoon</a> 健身平台！</span><em class="h2l"></em></h2>
-  <div class="column_c">
-   
-    <ul id="course_zdkc" class="zdkclist">
-    <!--[diy=jianshen_door_010_03]--><div id="jianshen_door_010_03" class="area"></div><!--[/diy]-->
-    </ul>
-
-    <!-- 活动-->
-    <div class="wydkc cl" style="clear:both"><!--[diy=jianshen_door_010_04]--><div id="jianshen_door_010_04" class="area"></div><!--[/diy]-->
-    </div>
-
-    <ul id="course_xyd" style="clear:both">
-    <!--[diy=jianshen_door_010_05]--><div id="jianshen_door_010_05" class="area"></div><!--[/diy]-->
-    </ul>
- 
-  </div>
-</div>
-
-<div class="right_block">
-  <h2><font style="font-size:18px; color:#333333; font-weight:bold;">滚动新闻</font> 每周都有更新</h2>
-    <!-- 滚动新闻 --><div class="newvideolist">
-<a id="newvideoGoLeft" class="up on" href="javascript:void(0)" target="_self"></a>
-<a id="newvideoGoRight" class="down on" href="javascript:void(0)" target="_self"></a>
-<div class="newvideolist_c">
-<ul id="newvideolist"><!--[diy=jianshen_door_010_06]--><div id="jianshen_door_010_06" class="area"></div><!--[/diy]--></ul>
-</div>
-</div>
-<!-- 滚动新闻 -->
-  </div>
-
-<div id="hjhy" class="column columnbug">
-    <div class="topbg2"></div>
-    <h2><span><a href="#" target="_blank">生活娱乐</a> 在这里可以找到你想要的东西，想找的信息</span><em class="h2l"></em></h2>
     <div class="column_c">
-    <div class="hy_c"><div class="zttj">
-<dl class="no1">    <dt>
-婚庆婚嫁</dt>
-<!--[diy=jianshen_door_010_07]--><div id="jianshen_door_010_07" class="area"></div><!--[/diy]-->
-    </dl>
-    <dl> <dt>
-旅游休闲</dt>
-<!--[diy=jianshen_door_010_08]--><div id="jianshen_door_010_08" class="area"></div><!--[/diy]-->
-    </dl><dl>    <dt>
-家居家装</dt>
-<!--[diy=jianshen_door_010_09]--><div id="jianshen_door_010_09" class="area"></div><!--[/diy]-->
-    </dl></div>
-</div>
-	    <div class="column_r">
-	    
-<h2 class="title_hj"><a href="#" title="贴子排行榜">贴子排行榜</a></h2>
-<ul class="phblist">
-<!--[diy=jianshen_door_010_10]--><div id="jianshen_door_010_10" class="area"></div><!--[/diy]-->
-</ul>
-
-	        <div class="ad_250_60"><!--[diy=jianshen_door_010_11]--><div id="jianshen_door_010_11" class="area"></div><!--[/diy]--></div>
-	    </div>
-      <ul class="cklist">
-       
-<!--[diy=jianshen_door_010_12]--><div id="jianshen_door_010_12" class="area"></div><!--[/diy]-->
-</ul>
+      
+    <div class="column_left_box">
+      <h2 class="title_hj"><a href="#" title="全民健身">全民健身</a></h2>
     </div>
-</div>
 
-<div id="zshy" class="column columnbug">
-    <div class="topbg2"></div>
-    <h2><span><a href="#" target="_blank">跳蚤市场</a> 这里有很多宝贝哦，亲，快来淘一淘，晒一晒咯！</span><em class="h2l"></em></h2>
-    <div class="column_c">
-    <div class="hy_c"><div class="zttj">
-<dl class="no1">    <dt>
-服装服饰</dt>
-<!--[diy=jianshen_door_010_13]--><div id="jianshen_door_010_13" class="area"></div><!--[/diy]-->
-       
-    </dl><dl>    <dt>
-二手货品</dt>
-  <!--[diy=jianshen_door_010_14]--><div id="jianshen_door_010_14" class="area"></div><!--[/diy]-->
-    </dl><dl>    <dt>
-晒一晒宝</dt>
-       <!--[diy=jianshen_door_010_15]--><div id="jianshen_door_010_15" class="area"></div><!--[/diy]-->
-    </dl></div>
-</div>
-    <div class="column_r">
-    
-<h2 class="title_hj"><a href="#" title="宝贝排行榜">宝贝排行榜</a></h2>
-<ul class="phblist">
-<!--[diy=jianshen_door_010_16]--><div id="jianshen_door_010_16" class="area"></div><!--[/diy]-->
-</ul>
-        <div class="ad_250_60"><!--[diy=jianshen_door_010_17]--><div id="jianshen_door_010_17" class="area"></div><!--[/diy]--></div>
-      </div>
-     <ul class="cklist">
-       <!--[diy=jianshen_door_010_18]--><div id="jianshen_door_010_18" class="area"></div><!--[/diy]-->
-
-</ul><!-- 钻石3图 e --> 
-    </div>
-</div>
-
-
-<div id="alfx" class="column columnbug">
-    <div class="topbg2"></div>
-    <h2><span><a href="#" target="_blank">分类信息</a> 这里包含了招聘信息，人才信息，房产信息，租房信息。</span><em class="h2l"></em></h2>
-    <div class="column_c">
-   <div class="hy_c" style="width:950px;"><div class="zttj">
-<dl class="no1" style="width:225px">    <dt>
-人才招聘</dt>
-<!--[diy=jianshen_door_010_19]--><div id="jianshen_door_010_19" class="area"></div><!--[/diy]-->
-       
-    </dl>
-    <dl style="width:225px">    <dt>
-租房信息</dt>
-  <!--[diy=jianshen_door_010_20]--><div id="jianshen_door_010_20" class="area"></div><!--[/diy]-->
-    </dl>
-    <dl style="width:225px">    <dt>
-交友中心</dt>
-       <!--[diy=jianshen_door_010_21]--><div id="jianshen_door_010_21" class="area"></div><!--[/diy]-->
-    </dl>
-    <dl style="width:225px"><dt>
-个人简历</dt>
-       <!--[diy=jianshen_door_010_22]--><div id="jianshen_door_010_22" class="area"></div><!--[/diy]-->
-    </dl></div>
-</div>
-</div>
-<div class="ad_950_90"><!--[diy=jianshen_door_010_23]--><div id="jianshen_door_010_23" class="area"></div><!--[/diy]--></div>
-<div class="column columnbug" style="padding-top:0px;">
-    <div class="column_c">
- <div class="column_tag">
-      <dl >
-       <!--[diy=jianshen_door_010_24]--><div id="jianshen_door_010_24" class="area"></div><!--[/diy]-->
-      </dl>
-    </div>
-    <div class="column_list">
-      <ul class="column_list_t">
-        <li class="on"><a target="_self" href="javascript:void(0)">房屋出租</a></li>
-        <li><a target="_self" href="javascript:void(0)">个人求职</a></li>
-        <li><a target="_self" href="javascript:void(0)">交友中心</a></li>
-        <li><a target="_self" href="javascript:void(0)">人才招聘</a></li>
-      </ul>
-      <div class="column_list_c">
-      <ul id="indexrank" style="display: block;">
- <!--[diy=jianshen_door_010_25]--><div id="jianshen_door_010_25" class="area"></div><!--[/diy]-->
-
-</ul>
-     <ul style="display: none;">
- <!--[diy=jianshen_door_010_26]--><div id="jianshen_door_010_26" class="area"></div><!--[/diy]-->
-</ul> 
-      <ul style="display: none;">
- <!--[diy=jianshen_door_010_27]--><div id="jianshen_door_010_27" class="area"></div><!--[/diy]-->
-</ul>
-
-      <ul style="display: none;">
- <!--[diy=jianshen_door_010_28]--><div id="jianshen_door_010_28" class="area"></div><!--[/diy]-->
-</ul>
- 
-      </div>
- </div>
-  <div class="column_jpzjtj">
-<h2><span style="margin-top:0px;">热门信息</span></h2>
-<ul class="jpzjtj_list">
- <!--[diy=jianshen_door_010_29]--><div id="jianshen_door_010_29" class="area"></div><!--[/diy]-->
-</ul>
-</div><!-- 精品专辑推荐4图 e  -->
+    <div class="hyfc">
+      <h2 class="title_hj"><a href="#" title="会员风采">会员风采</a></h2>
+      
   </div>
-  </div>
-  
-  
-  <div class="column columnbug">
-<div class="topbg2"></div>
-<h2><span><a href="#" target="_blank">活跃会员</a> 你可以在这里展示的你头相，认识更多的朋友哦，快来参加吧！</span><em class="h2l"></em></h2>
-<div class="column_c mar_0" style="width:960px;">  
-<div class="db_l jstd">
-<ul >
- <!--[diy=jianshen_door_010_30]--><div id="jianshen_door_010_30" class="area"></div><!--[/diy]-->
-</ul>
-</div>
-<div class="db_r jstd_txt">
-<div style="clear:both"><h3 class="title_a"><span class="t_jstxt">会员竞价展示</span></h3></div>
-<div style="clear:both">
- <!--[diy=jianshen_door_010_31]--><div id="jianshen_door_010_31" class="area"></div><!--[/diy]--></div>
 
 </div>
-</div>
-</div>
-
-<div class="video">	
-        <div class="topbg2"></div>
-       <div class="hd_s1">
-<span class="tit_tip"></span>
-<h2>
-<a class="hd_video" target="_blank" href="#">精美图片</a><em class="h2l"></em>
-</h2>
-<span class="menu_r">
-<a href="#">性感</a><a href="#">清纯</a><a href="#">可爱</a>|
-<a href="#">蓝天</a><a href="#">草原</a><a href="#">户外</a>|
-<a href="#"><strong>平面设计</strong></a><a href="#">作品</a><a href="#">欣赏</a><a href="#">创意</a></span>
-</div>
-        <div class="sdbox_lr_big video_active clearfix">
-        <span class="sdbox_bg_l"></span><span class="sdbox_bg_r"></span>
-<div id="videoplay">
-<div class="slides_container" style="overflow: hidden; position: relative;">
-
-<ul class="video_group" style="position: absolute; top: 0px;  z-index: 5; display: block; bottom: 0px;">
-<!--[diy=jianshen_door_010_32]--><div id="jianshen_door_010_32" class="area"></div><!--[/diy]-->
-</ul>
-
-<ul class="video_group" style="position: absolute; top: 0px; left: 832px; z-index: 0; display: none; bottom: 0px;">
- <!--[diy=jianshen_door_010_33]--><div id="jianshen_door_010_33" class="area"></div><!--[/diy]-->
-</ul>
-
-<ul class="video_group" style="position: absolute; top: 0px; left: 832px; z-index: 0; display: none; bottom: 0px;">
- <!--[diy=jianshen_door_010_34]--><div id="jianshen_door_010_34" class="area"></div><!--[/diy]-->
-</ul>
-
-
-</div>
-<a class="prev" href="#"></a>
-<a class="next" href="#"></a>
-</div> 
 
 
 
-<!--视频列表12条e-->
-		</div>        
-    </div>
-    
-    
-<div class="produced">
-  <div class="topbg2"></div>
-  <div class="hd_s1">
-    <h2 class="hd_produced" style="position: static; width:200px; clear:both">三味书屋</h2>
-  </div>
-  <ul class="clr">
-   <!--[diy=jianshen_door_010_35]--><div id="jianshen_door_010_35" class="area"></div><!--[/diy]-->
-  </ul>
-</div>
+
+
+
+
+
+
+
+
+
+
+
 
 <div class="buyguide">
-<div class="topbg2"></div>
-<ul class="glist">
-<!--[diy=jianshen_door_010_36]--><div id="jianshen_door_010_36" class="area"></div><!--[/diy]-->
-        	
-        </ul></div>
+  <div class="topbg2"></div>
+  <ul class="glist">
+  <!--[diy=jianshen_door_010_36]--><div id="jianshen_door_010_36" class="area"></div><!--[/diy]-->       	
+  </ul>
+</div>
         
         
-        <div class="column">
-<div class="topbg2"></div>
-<!--友情链接 --><div class="sdmain link clearfix">
-<span class="bottom_l"></span><span class="bottom_r"></span>
-<div class="hd_s1">
-<h2 style="position: static"><a class="hd_link" target="_blank" title="友情链接" href="#">友情链接</a></h2>
-</div>
-<div class="linkie6">
- <!--[diy=jianshen_door_010_37]--><div id="jianshen_door_010_37" class="area"></div><!--[/diy]-->
+<div class="column">
+  <div class="topbg2"></div>
+  <!--友情链接 -->
+  <div class="sdmain link clearfix">
+    <span class="bottom_l"></span><span class="bottom_r"></span>
+    <div class="hd_s1">
+      <h2 style="position: static"><a class="hd_link" target="_blank" title="友情链接" href="#">友情链接</a></h2>
+      </div>
+      <div class="linkie6">
+        <!--[diy=jianshen_door_010_37]--><div id="jianshen_door_010_37" class="area"></div><!--[/diy]-->
 
-</div>
-<style>
-*html .linkie6{ height:24px; line-height:24px;}
+      </div>
+    <style>
+    *html .linkie6{ height:24px; line-height:24px;}
 
-</style>
-</div><!--友情链接 -->
+    </style>
+  </div><!--友情链接 -->
 </div>
+
+
 </div>
 <div style="height:1px; clear:both"></div>
 <!--{template common/footer}-->
