fd52bdafb217a0dc7f007f2062e169b492595376
huangyuyao <huangyuyao@cd.tuan800.com>
2014-03-17 11:26:00 +0800

    modify

--- data/sysdata/cache_mobile.php
+++ data/sysdata/cache_mobile.php
@@ -1,6 +1,6 @@
 <?php
 //Discuz! cache file, DO NOT modify me!
-//Identify: 595d31e72c31122a86a0230c41bee959
+//Identify: e1018178aa7b50ee7b701ca53124d314
 
-$mobilecheck = '{"discuzversion":"X3.1","charset":"utf-8","version":"3","pluginversion":"1.3.1","oemversion":"0","regname":"register","qqconnect":"0","sitename":"\\u4f1a\\u6709\\u8fd0\\u52a8\\u7f51","mysiteid":"","ucenterurl":"http:\\/\\/localhost\\/jianshen\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
+$mobilecheck = '{"discuzversion":"X3.1","charset":"utf-8","version":"3","pluginversion":"1.3.1","oemversion":"0","regname":"register","qqconnect":"0","sitename":"\\u4f1a\\u6709\\u8fd0\\u52a8\\u7f51","mysiteid":"","ucenterurl":"http:\\/\\/www.yaophpwork.com\\/discuzx3\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
 ?>
